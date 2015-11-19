085f68a8bb57a623f956efab9fd12fa3fdb901e2
Aran Gilmore <aran@arangilmore.com>
2015-11-18 21:03:04 -0500

    Project Start

---
+++ js/assign3.js
@@ -0,0 +1,25 @@
+//Flashacard view and tester
+
+$(document).ready(function(){
+	//Array of known school names
+	var schArray = ["Harvard Extension School", "School for Engineering and Applied Sciences", "Graduate Schol of Education"]
+
+	$('#school').focusout(function(){
+		
+	}); //end school focusout
+
+}); //end ready
+
+
+
+
+
+
+
+
+
+
+
+
+
+
